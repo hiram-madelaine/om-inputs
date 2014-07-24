242c0a36130b7fcba09c1f9d7248518bcdd58acd
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2014-07-24 19:02:42 +0200

    advanced compilation works : Om 0.6.2 cljs 0.0-2268 async 0.1.303.0-886421-alpha

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject om-inputs "0.1.7"
+(defproject om-inputs "0.1.8"
   :description "Generate Web Input Form for Om/React.js, validation included."
   :url "https://github.com/hiram-madelaine/om-inputs"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2268"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.6.4"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [om "0.6.2"]
                  [prismatic/dommy "0.1.2"]
                  [prismatic/schema "0.2.4"]
                  [com.facebook/react "0.9.0"]
@@ -18,27 +18,26 @@
           :include [om-inputs.date-utils om-inputs.core]}
   :source-paths ["src"]
 
+
   :cljsbuild {
               :builds [{:id "dev"
                         :source-paths ["src"]
-                        :compiler {
-                                   :output-to "om_inputs.js"
+                        :compiler {:output-to "om_inputs.js"
                                    :output-dir "out"
                                    :optimizations :none
                                    :source-map true}}
                        {:id "simple"
                         :source-paths ["src"]
-                        :compiler {
-                                   :output-to "main.js"
+                        :compiler {:output-to "main.js"
                                    :optimizations :simple
                                    :pretty-print true
                                    :preamble ["react/react.min.js"]
                                    :externs ["react/externs/react.js"]}}
                        {:id "release"
                         :source-paths ["src"]
-                        :compiler {
-                                   :output-to "main.js"
+                        :compiler {:output-to "main.js"
                                    :optimizations :advanced
+                                   ;:closure-warnings {:check-useless-code :on}
                                    :pretty-print true
                                    :pseudo-names true
                                    :preamble ["react/react.min.js"]
