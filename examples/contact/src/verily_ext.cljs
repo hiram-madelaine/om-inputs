de89c97c5b7d23263c241798b5f7dab3c7e9ba47
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-09-22 11:36:07 +0200

    Comment out vat

--- examples/contact/src/verily_ext.cljs
+++ examples/contact/src/verily_ext.cljs
@@ -1,17 +1,18 @@
 (ns om-inputs.verily-ext
   (:require
     [jkkramer.verily :as v :refer [make-validator validation->fn]]
-    [clj-vat.algo :refer [check-ident]]))
+    ;[clj-vat.algo :refer [check-ident]]
+    ))
 
 
-(defn vat
+#_(defn vat
   [keys & [msg]]
   (make-validator
     keys #(not (check-ident %))
     (or msg "must be a valid VAT")))
 
 
-(defmethod validation->fn :vat
+#_(defmethod validation->fn :vat
   [vspec]
   (apply vat (rest vspec)))
 
