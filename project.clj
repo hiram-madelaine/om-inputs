ddf292f8e5a0c6f552bbb4493f7ba1af42eeb396
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-09-22 11:13:42 +0200

    FIX figwheel reload with :clean-target AND bump versions

--- project.clj
+++ project.clj
@@ -1,18 +1,19 @@
-(defproject om-inputs "0.3.9-SNAPSHOT"
+(defproject om-inputs "0.4.0-SNAPSHOT"
   :description "Generate Web Input Form for Om/React.js, validation included."
   :url "https://github.com/hiram-madelaine/om-inputs"
-
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
-                 [org.clojure/clojurescript "0.0-3308" :scope "provided"]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.omcljs/om "0.8.8" :scope "provided"]
+                 [org.omcljs/om "0.9.0"]
                  [prismatic/schema "0.4.2"]
                  [jkkramer/verily "0.6.0"]
-                 [sablono "0.2.22"]
-                 [clj-vat "0.1.2" :scope "provided"]]
+                 [sablono "0.3.4"]
+                 ;[clj-vat "0.1.2" :scope "provided"]
+                 ]
 
-  :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.3.3"]
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.0"]
             [codox "0.8.10"]]
 
   :min-lein-version "2.5.0"
@@ -22,34 +23,38 @@
   :jvm-opts ["-Xmx1g" "-server"]
 
   :codox {:language :clojurescript
-          :include [om-inputs.date-utils om-inputs.core]}
+          :include  [om-inputs.date-utils om-inputs.core]}
   :resource-paths ["examples"]
-
+  :test-paths ["test/cljs"]
+  :source-paths ["src" "examples/contact/src"]
+  :clean-targets ^{:protect false} ["examples/contact/out" "target"]
   :cljsbuild {
-              :builds [{:id "dev"
+              :builds [{:id           "dev"
                         :source-paths ["src" "examples/contact/src"]
-                        :compiler {:output-to "examples/contact/out/om_inputs.js"
-                                   :output-dir "examples/contact/out"
-                                   :optimizations :none
-                                   :source-map true}}
-                       {:id "simple"
+                        :figwheel     true
+                        :compiler     {:output-to     "examples/contact/out/om_inputs.js"
+                                       :output-dir    "examples/contact/out"
+                                       :asset-path    "contact/out"
+                                       :optimizations :none
+                                       :source-map    true}}
+                       {:id           "simple"
                         :source-paths ["src" "examples/contact/src"]
-                        :compiler {:output-to "examples/contact/out/main.js"
-                                   :optimizations :simple
-                                   :pretty-print true
-                                   :preamble ["react/react.min.js"]
-                                   :externs ["react/externs/react.js"]}}
-                       {:id "release"
+                        :compiler     {:output-to     "examples/contact/out/main.js"
+                                       :optimizations :simple
+                                       :pretty-print  true
+                                       :preamble      ["react/react.min.js"]
+                                       :externs       ["react/externs/react.js"]}}
+                       {:id           "release"
                         :source-paths ["src" "examples/contact/src"]
-                        :compiler {:output-to "examples/contact/out/main.js"
-                                   :optimizations :advanced
-                                   ;:closure-warnings {:check-useless-code :on}
-                                   :pretty-print false
-                                   :pseudo-names false
-                                   :preamble []
-                                   :externs []}}]}
+                        :compiler     {:output-to     "examples/contact/out/main.js"
+                                       :optimizations :advanced
+                                       ;:closure-warnings {:check-useless-code :on}
+                                       :pretty-print  false
+                                       :pseudo-names  false
+                                       :preamble      []
+                                       :externs       []}}]}
 
 
   :figwheel {:http-server-root "contact"
-             :server-port 3449
-             :css-dirs ["examples/contact/css"]})
+             :server-port      3450
+             :css-dirs         ["examples/contact/css"]})
